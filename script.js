df9046f141425d61f5458f2742a3f5920951492d
hbandi93 <hbandi93@gmail.com>
2021-10-26 15:42:34 +0200

    nav bar created.

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-function fastLog(text) {
-    console.log(text);
-    return "text logged";
-}
-
 function toggleButton(event) {
-    console.log(event.target);
-
     event.target.classList.toggle("clicked");
-    const fastLogResult = fastLog("Ezt a szöveget akarom kiloggoltatni");
-    console.log(fastLogResult);
+    document.getElementById("nav").classList.toggle("clicked");
 }
-
 
 function _load() {
     console.log("Page loaded.");
 
-    document.getElementById("menu-btn").addEventListener("click", toggleButton);;
+    document.getElementById("menu-btn").addEventListener("click", toggleButton);
 }
-
-
 
 window.addEventListener("load", _load);
