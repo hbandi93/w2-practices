6630591e2d249be6e9ae61bd8fbc1dca00231775
hbandi93 <hbandi93@gmail.com>
2021-10-26 11:23:38 +0200

    Click gives class to the button.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function _load() {
     console.log("Page loaded.");
 
+    document.getElementById("menu-btn").addEventListener("click", function (event) {
+        console.log(event.target);
+        event.target.classList.toggle("clicked");
+    });
+
 }
 
 window.addEventListener("load", _load);
